ebd5ac94d78239d845f6c59b750d2b0a6c8e5801
azizbekchoriyorqulov <azizbekchoriyorqulov54@gmail.com>
2024-01-25 13:37:01 +0500

    kmk;gm

---
+++ src/main/java/com/example/uzrailways/UzRailwaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.uzrailways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzRailwaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzRailwaysApplication.class, args);
+    }
+
+}

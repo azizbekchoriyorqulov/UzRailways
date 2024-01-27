93cf30851500ef7a4bdb043461456a34b4abe576
SuperHuma <theferganes@gmail.com>
2024-01-27 22:22:56 +0500

    jsh-sh-i-r

--- src/main/java/com/example/uzrailways/UzRailwaysApplication.java
+++ src/main/java/com/example/uzrailways/UzRailwaysApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UzRailwaysApplication {
-
-    public static void main(String[] args) {
+public class UzRailwaysApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(UzRailwaysApplication.class, args);
     }
-
+    //comm
 }

68ab29851a6520f3537ea8494ffe5f8d9e767630
azizbekchoriyorqulov <102897295+azizbekchoriyorqulov@users.noreply.github.com>
2024-01-28 02:53:35 +0500

    Revert "Davlat"

--- src/main/java/com/example/uzrailways/UzRailwaysApplication.java
+++ src/main/java/com/example/uzrailways/UzRailwaysApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UzRailwaysApplication
-{
-    public static void main(String[] args)
-    {
+public class UzRailwaysApplication {
+
+    public static void main(String[] args) {
         SpringApplication.run(UzRailwaysApplication.class, args);
     }
-    //comm
+
 }
